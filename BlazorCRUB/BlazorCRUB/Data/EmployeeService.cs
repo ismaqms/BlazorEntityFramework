8f53dc39e3837fa2bf41568f8493616e6e5fd338
Ismael Quijorna Mariblanca <alumno.193521@iesazarquiel.es>
2020-06-18 18:40:10 +0200

    Fin del proyecto

---
+++ BlazorCRUB/BlazorCRUB/Data/EmployeeService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUB.Data
+{
+    public class EmployeeService
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmployeeService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<EmployeeInfo> Getemployee()
+        {
+            var emplist = _db.Employees.ToList();
+            return emplist;
+        }
+
+        public string Create(EmployeeInfo objEmployee)
+        {
+            _db.Employees.Add(objEmployee);
+            _db.SaveChanges();
+            return "Save Successfully";
+        }
+
+        public EmployeeInfo GetEmployeeById(int id)
+        {
+            EmployeeInfo employee = _db.Employees.FirstOrDefault(s => s.EmployeeId == id);
+
+            return employee;
+        }
+
+        public string UpdateEmployee(EmployeeInfo objEmployee)
+        {
+            _db.Employees.Update(objEmployee);
+            _db.SaveChanges();
+            return "Update Successfully";
+        }
+
+        public string DeleteEmpInfo(EmployeeInfo objEmployee)
+        {
+            _db.Remove(objEmployee);
+            _db.SaveChanges();
+            return "Delete Successfully";
+        }
+    }
+}
--- BlazorCRUB/BlazorCRUB/Startup.cs
+++ BlazorCRUB/BlazorCRUB/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BlazorCRUB.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace BlazorCRUB
 {
@@ -29,6 +30,9 @@ namespace BlazorCRUB
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<EmployeeService>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
